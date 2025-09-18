5e852ed440da90969dbc3faca71f25fce6e83d24
gooblet234 <awesomerocket1000@gmail.com>
2025-09-18 11:01:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,11 @@
     const storedPassword = sessionStorage.getItem('password');
     const storedPassword2 = sessionStorage.getItem('password2');
 
-    const isPassword1Correct = storedPassword && encodedPasswords.includes(storedPassword);
+    const isPasswordCorrect = storedPassword && encodedPasswords.includes(storedPassword);
     const isPassword2Correct = storedPassword2 && encodedPasswords2.includes(storedPassword2);
-
-    if (!isPassword1Correct || !isPassword2Correct) {
+    
+    if (!isPasswordCorrect || !isPassword2Correct) {
       alert("Access denied. Please log in.");
       window.location.href = "index.html"; 
     }
-    sessionStorage.clear();
   });
