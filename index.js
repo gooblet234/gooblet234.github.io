0e2cd5659054822ec6a2e044a42e41e0dc3fa94b
gooblet234 <awesomerocket1000@gmail.com>
2025-09-18 10:56:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-    window.addEventListener('DOMContentLoaded', (event) => {
-      const encodedPasswords = ["MjM0MTQyMzQ=", "R01TMjAyNQ==", "bGltYnVzY29tcGFueQ=="];
-      const encodedPasswords2 = ["cGll"];
-      
-      const storedPassword = sessionStorage.getItem('password');
-      const storedPassword2 = sessionStorage.getItem('password2');
+  window.addEventListener('DOMContentLoaded', (event) => {
+    const encodedPasswords = ["MjM0MTQyMzQ=", "R01TMjAyNQ==", "bGltYnVzY29tcGFueQ=="];
+    const encodedPasswords2 = ["cGll"];
+    
+    const storedPassword = sessionStorage.getItem('password');
+    const storedPassword2 = sessionStorage.getItem('password2');
 
-      if (storedPassword && encodedPasswords.includes(storedPassword) &&
-          storedPassword2 && encodedPasswords2.includes(storedPassword2)) {
-        sessionStorage.clear();
-        alert("Access denied. Please log in.");
-        window.location.href = "index.html"; 
-      }
-    });
+    const isPassword1Correct = storedPassword && encodedPasswords.includes(storedPassword);
+    const isPassword2Correct = storedPassword2 && encodedPasswords2.includes(storedPassword2);
+
+    if (!isPassword1Correct || !isPassword2Correct) {
+      alert("Access denied. Please log in.");
+      window.location.href = "index.html"; 
+    }
+    sessionStorage.clear();
+  });
