0dbc8aa6423858ce7c4fbb0d74a240d9d5a520af
gooblet234 <awesomerocket1000@gmail.com>
2025-09-18 09:31:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
     window.addEventListener('DOMContentLoaded', (event) => {
       const encodedPasswords = ["MjM0MTQyMzQ=", "R01TMjAyNQ==", "bGltYnVzY29tcGFueQ=="];
+      const encodedPasswords2 = ["cGll"];
       
       const storedPassword = sessionStorage.getItem('password');
+      const storedPassword2 = sessionStorage.getItem('password2');
 
-      if (!storedPassword || !encodedPasswords.includes(storedPassword)) {
+      if (storedPassword && encodedPasswords.includes(storedPassword) &&
+          storedPassword2 && encodedPasswords2.includes(storedPassword2)) {
         sessionStorage.clear();
         alert("Access denied. Please log in.");
         window.location.href = "index.html"; 
